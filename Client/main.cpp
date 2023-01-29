ab3a4eee9723c601946945d9a952e87652025544
Fedoseev Viacheslav <lslava121001l@gmail.com>
2023-01-29 13:04:43 +0300

    Добавьте файлы проекта.

---
+++ Client/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+int main()
+{
+    WSAData wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        cout << "Error Startup\n";
+        return 1;
+    }
+
+    sockaddr_in clientSocket;
+    int clientSocketLen = sizeof(clientSocket);
+    clientSocket.sin_addr.s_addr = inet_addr("127.0.0.1");
+    clientSocket.sin_port = htons(8888);
+    clientSocket.sin_family = AF_INET;
+
+    SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if (connect(Connection, (SOCKADDR*)&clientSocket, clientSocketLen) != 0)
+    {
+        cout << "Error connet\n";
+        return 1;
+    }
+    cout << "Connected\n";
+
+    thread t = thread([&]()
+        {
+            char buf[256];
+            while (true)
+            {
+                int a = recv(Connection, buf, sizeof(buf), NULL);
+                if (a > 0)
+                    cout << buf << endl;
+            }
+        });
+
+    while (true)
+    {
+        char msg[256];
+        cin >> msg;
+        send(Connection, msg, sizeof(msg), NULL);
+    }
+
+    system("pause");
+    WSACleanup();
+    closesocket(Connection);
+}
+
---
+++ Server/main.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+thread thr[100];
+SOCKET ListConnected[100];
+int Counter = 0;
+
+int main()
+{
+    WSAData wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        cout << "Error Startup\n";
+        return 1;
+    }
+
+    sockaddr_in serverSocket;
+    int serverSocketLen = sizeof(serverSocket);
+    serverSocket.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverSocket.sin_port = htons(8888);
+    serverSocket.sin_family = AF_INET;
+
+    SOCKET socketListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(socketListen, (sockaddr*)&serverSocket, serverSocketLen);
+    listen(socketListen, SOMAXCONN);
+
+    for (int i = 0; i < 100; ++i)
+    {
+        SOCKET newConnnection = accept(socketListen, (sockaddr*)&serverSocket, &serverSocketLen);
+
+
+        if (newConnnection == 0)
+        {
+            cout << "Error new connection\n";
+        }
+        else
+        {
+            cout << "Client connected\n";
+
+            ListConnected[Counter] = newConnnection;
+
+
+            thr[Counter] = thread([&]()
+                {
+                    char buf[256];
+                    int ii = Counter;
+                    while (true)
+                    {
+                        if (recv(ListConnected[ii], buf, sizeof(buf), NULL) > 0)
+                        {
+                            for (int i = 0; i < Counter; ++i)
+                            {
+                                if (ii == i)
+                                    continue;
+
+                                send(ListConnected[i], buf, sizeof(buf), NULL);
+                            }
+                        }
+                    }
+                });
+
+            Sleep(10);
+
+            ++Counter;
+        }
+    }
+
+    system("pause");
+    WSACleanup();
+    closesocket(socketListen);
+    for (int i = 0; i < Counter; ++i)
+    {
+        thr[i].join();
+        closesocket(ListConnected[i]);
+    }
+}
